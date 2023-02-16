b6f1d7735e75e4e5a5936adc9a5d7c92225eca04
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-16 17:24:13 +0100

    rotating a matrix clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Rotates a list lists clockwise"""
 
 
 def rotate_2d_matrix(matrix):
+    """function to rotate the matrix"""
     return [list(row[::-1]) for row in zip(*matrix)]
