dfa74546c8d85fdef691ea48c22b4437aea27c13
Bolaji581 <oyedelebolaji84@gmail.com>
2023-02-27 05:42:32 +0100

    total coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
+    """
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        num_coins += total // coin
+        total %= coin
+    if total != 0:
+        return -1
+    return num_coins
